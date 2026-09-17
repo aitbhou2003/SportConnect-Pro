5c83ae14f27204881019c2d9a269f473586f55e2
aitbhou2003 <aitbhou.abdellah@gmail.com>
2026-09-17 14:44:30 +0100

    modifier la reuquet qui retourner touts les membre

--- src/services/memberService.js
+++ src/services/memberService.js
@@ -2,8 +2,20 @@ const pool = require("../config/database")
 
 async function getAllMenmbers() {
     const result = await pool.query(`
-        SELECT * FROM members 
-        ORDER BY id
+        SELECT 
+        members.id,
+        members.name,
+        members.birth_date,
+        members.resident,
+        members.qf,
+        members.medical_certificate_date,
+        members.family_id,
+        families.name AS family_name
+        
+        FROM members 
+        JOIN families
+                    ON members.family_id = families.id
+        ORDER BY members.id
         `);
 
     return result.rows;
